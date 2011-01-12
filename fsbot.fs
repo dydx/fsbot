0ced06f51f42dc0941af926d7cdb7829afd368b6
Josh Sandlin <dydx@thenullbyte.org>
2011-01-11 19:15:14 -0500

    bot works. added some small interactive features to make it more sociable. still not very talkative.

--- fsbot.fs
+++ fsbot.fs
@@ -11,7 +11,7 @@ open System.Net
 open System.Net.Sockets
 
 let server  = "irc.enigmagroup.org"
-let port    = 6697
+let port    = 6667
 let channel = "#bots"
 let nick    = "ishbot"
 
@@ -22,14 +22,11 @@ type IRCClient( h : string, p : int, c : string, n : string ) =
   let chan = c
   let nick = n
   let conn = new TcpClient()
+  do conn.Connect( host, port )
 
   // gather up the stream processing stuffs
   let sr = new StreamReader( conn.GetStream() )
   let sw = new StreamWriter( conn.GetStream() )
-  
-  // connect to the server
-  member this.Connect =
-    conn.Connect( host, port )
 
   member this.Pong =
     sw.WriteLine( sprintf "PONG %s\n" host)
@@ -57,7 +54,6 @@ type IRCClient( h : string, p : int, c : string, n : string ) =
     sw.Flush()
 
   member this.Run =
-    do this.Connect   // connect to server
     do this.Identify // identify with server
     do this.Join      // join a room
     Console.WriteLine( sprintf "Successfully joined %s on %s:%d as %s\n" chan host port nick)
@@ -65,9 +61,19 @@ type IRCClient( h : string, p : int, c : string, n : string ) =
       let line = sr.ReadLine()
       if (line.Contains("PING")) then
         this.Pong
+        
       let msg = line.Substring( line.Substring(1).IndexOf(":") + 2)
-      if (msg.Contains("*help")) then
-        this.Privmsg( "What do you want now?" )
+      let (|Prefix|_|)(p:string)(s:string) =
+        if s.StartsWith( p ) then
+          Some( s.Substring(p.Length))
+        else
+          None
+      match msg with
+      | Prefix "!version" rest -> this.Privmsg ( sprintf "%s on %A" nick Environment.Version )
+      | Prefix "!date" rest -> this.Privmsg ( sprintf "%A" System.DateTime.Now )
+      | Prefix "!help" rest -> this.Privmsg "Google it for now..."
+       
+
 
 // Lets actually use this badboy!
 let fsbot = new IRCClient( server, port, channel, nick )
